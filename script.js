d54ca22e1765e72c230d2fee5c2551b003504f80
Eric <eric.singelmann@gmx.de>
2021-11-02 21:19:06 +0100

    Questions added

---
+++ script.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+        question: "Chihuahuas sind die kleinste Hunderasse der Welt. Aber sie sind auch...?",
+        answer1: "die lauteste Hunderasse der Welt",
+        answer2: "eine der schnellsten Hunderassen der Welt",
+        answer3: "eine der ältesten Hunderassen der Welt",
+        answer4: "die aggressivte Hunderasse der Welt",
+        right_answer: 3
+    },
+    {
+        question: "Bei welchen Vögeln wird die harte, schildförmige Spitze des Oberschnabels Nagel genannt?",
+        answer1: "Pelikane",
+        answer2: "Enten",
+        answer3: "Gänse",
+        answer4: "Flamingos",
+        right_answer: 2
+    },
+    {
+        question: 'Unter "Arthropoden" versteht man...?',
+        answer1: "Kopffüßer",
+        answer2: "Gliederfüßer",
+        answer3: "Würmer",
+        answer4: "Krabbentiere",
+        right_answer: 2
+    },
+    {
+        question: 'Zu welcher Tiergattung gehört die "Scardafella"?',
+        answer1: "Pfauen",
+        answer2: "Enten",
+        answer3: "Hühnern",
+        answer4: "Tauben",
+        right_answer: 4
+    },
+    {
+        question: 'Zu welcher Vogelfamilie gehört der "Lachende Hans"?',
+        answer1: "Kolibris",
+        answer2: "Eisvögel",
+        answer3: "Wellensittiche",
+        answer4: "Raben",
+        right_answer: 2
+    }
+]
